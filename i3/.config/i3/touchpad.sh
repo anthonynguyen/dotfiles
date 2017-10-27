419978e63112b8684328c7e4bc74cadd849219e5
Anthony Nguyen <anknguyen@gmail.com>
2017-10-27 09:40:12 -0400

    Fix touchpad script

--- i3/.config/i3/touchpad.sh
+++ i3/.config/i3/touchpad.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 touchpad_id=$(xinput list | grep -i touchpad | awk '{print $7}' | awk -F"=" '{print $2}')
+tap_enabled=$(xinput list-props $touchpad_id | grep 'Tapping Enabled (' | awk -F'[)(]' '{print $2}')
 
-xinput set-prop $touchpad_id 295 1
+xinput set-prop $touchpad_id $tap_enabled 1
